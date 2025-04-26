4a562742d7babfde7289a658653b084ac391d1a5
andreeeestor <andrenestor0503@gmail.com>
2025-04-26 13:42:55 -0300

    feat: create routes file at App.tsx closes #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import Waves from "./components/animations/Waves";
-import Header from "./components/layout/Header";
-import MainContainer from "./components/layout/MainContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Header />
-      <MainContainer>
-        <div className="bg-black rounded-3xl shadow-2xl h-96 relative">
-          <Waves
-            lineColor="#fff"
-            backgroundColor="#000"
-             waveSpeedX={0.02}
-             waveSpeedY={0.01}
-             waveAmpX={40}
-             waveAmpY={20}
-             friction={0.9}
-             tension={0.01}
-             maxCursorMove={120}
-             xGap={12}
-             yGap={36}
-          />
-        </div>
-      </MainContainer>
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,50 @@
+import Waves from "../../components/animations/Waves";
+import Header from "../../components/layout/Header";
+import MainContainer from "../../components/layout/MainContainer";
+import Button from "../../components/shared/Button";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <MainContainer>
+        <section className="bg-[#03172C] rounded-3xl shadow-2xl h-96 relative">
+          <Waves
+            lineColor="#228EEA"
+            waveSpeedX={0.02}
+            waveSpeedY={0.01}
+            waveAmpX={40}
+            waveAmpY={20}
+            friction={0.9}
+            tension={0.01}
+            maxCursorMove={120}
+            xGap={12}
+            yGap={36}
+          />
+          <div className="flex flex-col gap-y-6 justify-center items-center h-full z-50 text-white">
+            <h1 className="text-3xl font-extrabold text-white sm:text-5xl italic">
+              second mind.
+            </h1>
+            <p className="text-center text-gray-300">
+              Automatizando Processos <br />
+              Transformando desafios em códigos <br />
+              Sua solução no desenvolvimento de softwares <br />
+              Potencializando seu sucesso digital <br />
+              Inovação sob medida
+            </p>
+            <div className="flex md:flex-row flex-col items-center gap-6">
+              <Button
+                text="Precificador de Projetos"
+                className="border-[#03a9f4] bg-[#03a9f4] hover:text-white before:bg-[#03a9f4] md:w-auto w-full"
+              />
+              <Button
+                text="Saiba Mais"
+                className="hover:text-black before:bg-white md:w-auto w-full"
+              />
+            </div>
+          </div>
+        </section>
+      </MainContainer>
+    </>
+  );
+}
