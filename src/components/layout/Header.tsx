9221c3ea9abf41af77fcb7785cdce4db24251179
andreeeestor <andrenestor0503@gmail.com>
2025-04-26 13:51:48 -0300

    feat: Responsive menu for header created

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,24 +1,25 @@
-import Logo from "../../assets/images/logo.png"
+import { List } from "@phosphor-icons/react";
+import Logo from "../../assets/images/logo.png";
 
-
-export default function Header(){
-    return(
-        <header className="py-3 px-6 flex items-center justify-between">
-           <div>
-            <img src={Logo} width={70} alt="logo" />
-           </div>
-           <nav>
-                <ul className="list-none uppercase md:flex hidden gap-x-6 transition-all">
-                    <li className="">Início</li>
-                    <li className="">Sobre</li>
-                    <li className="">Outsourcing</li>
-                    <li className="">Produtos</li>
-                    <li className="">Serviços</li>
-                    <li className="">Clientes</li>
-                    <li className="">News</li>
-                    <li className="">Canais</li>
-                </ul>
-            </nav> 
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="py-3 px-6 flex items-center justify-between">
+      <div>
+        <img src={Logo} width={70} alt="logo" />
+      </div>
+      <nav>
+        <List size={32} weight="bold" className="block md:hidden" />
+        <ul className="list-none uppercase md:flex hidden gap-x-6 transition-all">
+          <li className="">Início</li>
+          <li className="">Sobre</li>
+          <li className="">Outsourcing</li>
+          <li className="">Produtos</li>
+          <li className="">Serviços</li>
+          <li className="">Clientes</li>
+          <li className="">News</li>
+          <li className="">Canais</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
