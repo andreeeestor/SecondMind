4801818c929d9f34aa5fdc738db8dc52e0c68acf
andreeeestor <andrenestor0503@gmail.com>
2025-04-28 14:44:15 -0300

    feat: adding the service card at service section

--- src/components/cards/ServicesCard.tsx
+++ src/components/cards/ServicesCard.tsx
@@ -1,11 +1,51 @@
-import { Icon } from "@phosphor-icons/react"
+import { ArrowUpRight, Icon } from "@phosphor-icons/react";
 
 interface ServiceCardProps {
-    text: string
-    Icon: Icon
+  title: string;
+  description: string;
+  Icon: Icon;
+  side: "right" | "left";
 }
-export default function ServiceCard(props : ServiceCardProps){
-    return(
-        <></>
-    )
+export default function ServiceCard({
+  title,
+  description,
+  Icon,
+  side,
+}: ServiceCardProps) {
+  return (
+    <>
+      {side == "left" ? (
+        <div className="flex items-center border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+          <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-[#03a9f4] flex-shrink-0">
+            <Icon className="sm:w-16 sm:h-16 w-10 h-10" color="white" />
+          </div>
+          <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+            <h2 className=" text-lg title-font font-semibold mb-2">{title}</h2>
+            <p className="leading-relaxed text-gray-400">{description}</p>
+            <a className="mt-3 text-sky-500 inline-flex items-center transition-all hover:opacity-90 hover:underline cursor-pointer">
+              Leia Mais
+              <ArrowUpRight className="size-4 ml-2" />
+            </a>
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+          <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 sm:hidden inline-flex items-center justify-center rounded-full bg-[#03a9f4] flex-shrink-0">
+            <Icon className="sm:w-16 sm:h-16 w-10 h-10" color="white" />
+          </div>
+          <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+            <h2 className=" text-lg title-font font-semibold mb-2">{title}</h2>
+            <p className="leading-relaxed text-gray-400">{description}</p>
+            <a className="mt-3 text-sky-500 inline-flex items-center transition-all hover:opacity-90 hover:underline cursor-pointer">
+              Leia Mais
+              <ArrowUpRight className="size-4 ml-2" />
+            </a>
+          </div>
+          <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 hidden sm:inline-flex items-center justify-center rounded-full bg-[#03a9f4] flex-shrink-0 ">
+            <Icon className="sm:w-16 sm:h-16 w-10 h-10" color="white" />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,5 +1,4 @@
 import {
-  ArrowUpRight,
   ChartBar,
   CloudArrowUp,
   Database,
@@ -7,6 +6,7 @@ import {
   FileCode,
   Link,
 } from "@phosphor-icons/react";
+import ServiceCard from "../cards/ServicesCard";
 
 interface ServicesProps {
   id?: string;
@@ -14,114 +14,45 @@ interface ServicesProps {
 export default function Services(props: ServicesProps) {
   return (
     <section id={props.id}>
-        <div className="flex items-center border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-          <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-[#03a9f4] flex-shrink-0">
-            <Database className="sm:w-16 sm:h-16 w-10 h-10" color="white" />
-          </div>
-          <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-            <h2 className=" text-lg title-font font-semibold mb-2">Big Data</h2>
-            <p className="leading-relaxed text-gray-400">
-              Adquira insights valiosos de suas tendências de mercado,
-              transformando suas informações em dados.
-            </p>
-            <a className="mt-3 text-sky-500 inline-flex items-center transition-all hover:opacity-90 hover:underline cursor-pointer">
-              Leia Mais
-              <ArrowUpRight className="size-4 ml-2" />
-            </a>
-          </div>
-        </div>
-
-        <div className="flex items-center border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-          <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-            <h2 className=" text-lg title-font font-semibold mb-2">
-              Aplicações Web
-            </h2>
-            <p className="leading-relaxed text-gray-400">
-              Tenha softwares sob medida para seus processos, aumentando a
-              produtividade do seu negócio.
-            </p>
-            <a className="mt-3 text-sky-500 inline-flex items-center transition-all hover:opacity-90 hover:underline cursor-pointer">
-              Leia Mais
-              <ArrowUpRight className="size-4 ml-2" />
-            </a>
-          </div>
-          <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-[#03a9f4] flex-shrink-0">
-            <FileCode className="sm:w-16 sm:h-16 w-10 h-10" color="white" />
-          </div>
-        </div>
-
-        <div className="flex items-center border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-          <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-[#03a9f4] flex-shrink-0">
-            <DeviceMobile className="sm:w-16 sm:h-16 w-10 h-10" color="white" />
-          </div>
-          <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-            <h2 className=" text-lg title-font font-semibold mb-2">
-              Aplicativos
-            </h2>
-            <p className="leading-relaxed text-gray-400">
-              Se sua empresa necessita de um aplicativo para ampliar seu mercado
-              ou para tratar melhor seus processos.
-            </p>
-            <a className="mt-3 text-sky-500 inline-flex items-center transition-all hover:opacity-90 hover:underline cursor-pointer">
-              Leia Mais
-              <ArrowUpRight className="size-4 ml-2" />
-            </a>
-          </div>
-        </div>
-
-        <div className="flex items-center border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-          <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-            <h2 className=" text-lg title-font font-semibold mb-2">
-              APIs
-            </h2>
-            <p className="leading-relaxed text-gray-400">
-            Leia dados de outras aplicações, disponibilize seus dados para parceiros, conecte diferentes áreas da sua empresa.
-            </p>
-            <a className="mt-3 text-sky-500 inline-flex items-center transition-all hover:opacity-90 hover:underline cursor-pointer">
-              Leia Mais
-              <ArrowUpRight className="size-4 ml-2" />
-            </a>
-          </div>
-          <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-[#03a9f4] flex-shrink-0">
-            <CloudArrowUp className="sm:w-16 sm:h-16 w-10 h-10" color="white" />
-          </div>
-        </div>
-
-        <div className="flex items-center border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-          <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-[#03a9f4] flex-shrink-0">
-            <ChartBar className="sm:w-16 sm:h-16 w-10 h-10" color="white" />
-          </div>
-          <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-            <h2 className=" text-lg title-font font-semibold mb-2">
-              Reports
-            </h2>
-            <p className="leading-relaxed text-gray-400">
-            Dashboards e repots possibilitando acesso direto aos seus dados mais importantes.
-            </p>
-            <a className="mt-3 text-sky-500 inline-flex items-center transition-all hover:opacity-90 hover:underline cursor-pointer">
-              Leia Mais
-              <ArrowUpRight className="size-4 ml-2" />
-            </a>
-          </div>
-        </div>
-
-        <div className="flex items-center border-b pb-10 border-gray-200 sm:flex-row flex-col">
-          <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-            <h2 className=" text-lg title-font font-semibold mb-2">
-            Integração de Sistemas
-            </h2>
-            <p className="leading-relaxed text-gray-400">
-            Integre seus sistemas e serviços de forma transparente e funcional.
-            </p>
-            <a className="mt-3 text-sky-500 inline-flex items-center transition-all hover:opacity-90 hover:underline cursor-pointer">
-              Leia Mais
-              <ArrowUpRight className="size-4 ml-2" />
-            </a>
-          </div>
-          <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-[#03a9f4] flex-shrink-0">
-            <Link className="sm:w-16 sm:h-16 w-10 h-10" color="white" />
-          </div>
-        </div>
+      <ServiceCard
+        title="Big Data"
+        description="Adquira insights valiosos de suas tendências de mercado,
+              transformando suas informações em dados."
+        Icon={Database}
+        side="left"
+      />
+      <ServiceCard
+        title="Aplicações Web"
+        description="Tenha softwares sob medida para seus processos, aumentando a
+              produtividade do seu negócio."
+        Icon={FileCode}
+        side="right"
+      />
+      <ServiceCard
+        title="Aplicativos"
+        description="Se sua empresa necessita de um aplicativo para ampliar seu mercado
+              ou para tratar melhor seus processos."
+        Icon={DeviceMobile}
+        side="left"
+      />
+      <ServiceCard
+        title="APIs"
+        description="Leia dados de outras aplicações, disponibilize seus dados para parceiros, conecte diferentes áreas da sua empresa."
+        Icon={CloudArrowUp}
+        side="right"
+      />
+      <ServiceCard
+        title="Reports"
+        description="Dashboards e repots possibilitando acesso direto aos seus dados mais importantes."
+        Icon={ChartBar}
+        side="left"
+      />
+      <ServiceCard
+        title="Integração de Sistemas"
+        description="Integre seus sistemas e serviços de forma transparente e funcional."
+        Icon={Link}
+        side="right"
+      />
     </section>
   );
 }
