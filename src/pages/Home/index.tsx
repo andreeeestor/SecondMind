2c00439648ff976079623b80753d5e4e94a4414d
andreeeestor <andrenestor0503@gmail.com>
2025-04-28 16:26:33 -0300

    feat: Hero component created closes #2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import Waves from "../../components/animations/Waves";
+import Hero from "../../components/layout/Hero";
 import About from "../../components/sections/About";
 import Contact from "../../components/sections/Contact";
 import News from "../../components/sections/News";
@@ -7,47 +7,11 @@ import Products from "../../components/sections/Products";
 import Resources from "../../components/sections/Resources";
 import Services from "../../components/sections/Services";
 import Testimonials from "../../components/sections/Testimonials";
-import Button from "../../components/shared/Button";
 
 export default function HomePage() {
   return (
     <>
-      <section className="bg-[#03172C] rounded-3xl shadow-2xl h-96 relative">
-        <Waves
-          lineColor="#228EEA"
-          waveSpeedX={0.02}
-          waveSpeedY={0.01}
-          waveAmpX={40}
-          waveAmpY={20}
-          friction={0.9}
-          tension={0.01}
-          maxCursorMove={120}
-          xGap={12}
-          yGap={36}
-        />
-        <div className="flex flex-col gap-y-6 justify-center items-center h-full z-50 text-white">
-          <h1 className="text-3xl font-extrabold text-white sm:text-5xl italic">
-            second mind.
-          </h1>
-          <p className="text-center text-gray-300">
-            Automatizando Processos <br />
-            Transformando desafios em códigos <br />
-            Sua solução no desenvolvimento de softwares <br />
-            Potencializando seu sucesso digital <br />
-            Inovação sob medida
-          </p>
-          <div className="flex md:flex-row flex-col items-center gap-6">
-            <Button
-              text="Precificador de Projetos"
-              className="border-[#03a9f4] bg-[#03a9f4] text-white hover:text-white before:bg-[#03a9f4] md:w-auto w-full"
-            />
-            <Button
-              text="Saiba Mais"
-              className="hover:text-black before:bg-white md:w-auto w-full text-white"
-            />
-          </div>
-        </div>
-      </section>
+      <Hero />
       <div className="md:h-20 h-6" />
       <About />
       <div className="md:h-20 h-6" />
